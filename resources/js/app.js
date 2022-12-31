f9679abb972f7b527f4d594d71c47f27d304ffe0
Antonio Bruno <abruno@artiso.com>
2022-12-31 17:24:22 +0100

    fix resources missing in Vite manifest

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
 import * as bootstrap from 'bootstrap'
 import '../sass/app.scss';
 
+// Needed to get assets after build : https://laravel.com/docs/9.x/vite#blade-processing-static-assets
+import.meta.glob([
+    '../images/**',
+    '../fonts/**',
+]);
+
 const myCarouselElement = document.getElementById('carouselExampleCaptions')
+
+// TODO: to be checked
 const carousel = new bootstrap.Carousel(myCarouselElement, {
     interval: 6000,
     wrap: true

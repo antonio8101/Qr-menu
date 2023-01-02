bf6d117559244063effe04267cb20544043a7c76
Luca Casella <filoluca92@gmail.com>
2023-01-02 21:19:17 +0100

    public ui in components and fixed carousel

---
+++ resources/views/components/public-components/footer-component.blade.php
@@ -0,0 +1,48 @@
+@php use Illuminate\Support\Facades\Vite; @endphp
+<footer>
+    <div class="container">
+        <div class="footerHeaders row">
+            <h6 class="text-center col-4 col-xs-5">
+                Servizi
+            </h6>
+            <h6 class="text-center col-4 col-xs-5">
+                Assistenza
+            </h6>
+        </div>
+        <div class="support row d-flex">
+            <ul class="col-4 text-center col-xs-5">
+                <li>
+                    <a href="#intro">Home</a>
+                </li>
+                <li>
+                    <a href="#features">Come funziona</a>
+                </li>
+                <li>
+                    <a href="#benefits">Vantaggi</a>
+                </li>
+                <li>
+                    <a href="#pricing">Prezzi</a>
+                </li>
+                <li>
+                    <a href="#faq">Faq</a>
+                </li>
+            </ul>
+            <div class="col-4 text-center col">
+                Email: <a href="">info@qrmenu.it</a>
+            </div>
+            <div class="col-4 col-xs-6">
+                <ul>
+                    <li>
+                        <a href="">Privacy policy</a>
+                    </li>
+                    <li>
+                        <a href="">Cookie policy</a>
+                    </li>
+                    <li>
+                        <a href="">Termini di servizio</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>

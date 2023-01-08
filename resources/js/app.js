1e6468e71e3a89328bc972f51db37c12a2804bc6
Luca Casella <filoluca92@gmail.com>
2023-01-09 00:51:02 +0100

    fixed broken layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,21 @@
 import './bootstrap';
+import '../sass/app.scss';
 
+// Needed to get assets after build : https://laravel.com/docs/9.x/vite#blade-processing-static-assets
+import.meta.glob([
+    '../images/**',
+    '../fonts/**',
+]);
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const myCarouselElement = document.getElementById('carouselExampleCaptions')
+
+// TODO: to be checked
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 6000,
+    wrap: true
+})
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import * as path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
+    resolve:{
+        alias:{
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });

b300173856d90780bb88648c088f497d01a3291e
Luca Casella <filoluca92@gmail.com>
2023-01-09 10:22:36 +0100

    add app.css to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/js/app.js',
+                'resources/css/app.css',
+                'resources/js/app.js'
             ],
             refresh: true,
         }),

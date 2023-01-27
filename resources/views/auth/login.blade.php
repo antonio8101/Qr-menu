967f77c2e78d95b31b42b9cacd5c8d1c180db135
Luca Casella <filoluca92@gmail.com>
2023-01-27 17:55:45 +0100

    added sign up to login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,8 +34,12 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 me-3" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
+                </a>
+
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500" href="{{ route('register') }}">
+                    {{ __('Sign up') }}
                 </a>
             @endif
 

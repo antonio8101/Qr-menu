a3b21bf42c295b89f99d0e8ea09cccc4c9781d67
Antonio Bruno <abruno@artiso.com>
2023-01-30 17:14:57 +0100

    point to build scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width,initial-scale=1"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="To be refined"/>
-    <link rel="manifest" href="/manifest.json"/>
+{{--    <link rel="manifest" href="/manifest.json"/>--}}
     <title>PrivateArea - {{ config('app.name', 'Laravel') }}</title>
-    <script defer="defer" src="/static/js/main.d753011c.js"></script>
-    <link href="/static/css/main.e6c13ad2.css" rel="stylesheet">
+{{--    <script defer="defer" src="/static/js/main.d753011c.js"></script>--}}
+{{--    <link href="/static/css/main.e6c13ad2.css" rel="stylesheet">--}}
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['public/build-privatearea/static/css/main.css', 'public/build-privatearea/static/js/main.js'])
 </head>
 <body>
 <noscript>You need to enable JavaScript to run this app.</noscript>

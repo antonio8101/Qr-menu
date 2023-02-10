34e617aea524e7c440662190b897aa98f11c6fc5
Antonio Bruno <abruno@artiso.com>
2023-02-10 15:36:49 +0100

    working with browser localstorage

--- resources/app/src/localStorage.js
+++ resources/app/src/localStorage.js
@@ -14,3 +14,27 @@ export function setUser(user) {
 export function removeUser() {
     localStorage.removeItem(USER_KEY);
 }
+
+export function getAccessToken() {
+    return localStorage.getItem(ACCESS_TOKEN_KEY);
+}
+
+export function removeAccessToken() {
+    localStorage.removeItem(ACCESS_TOKEN_KEY);
+}
+
+export function setAccessToken(accessToken) {
+    localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+}
+
+export function getRefreshToken() {
+    return localStorage.getItem(REFRESH_TOKEN_KEY);
+}
+
+export function removeRefreshToken() {
+    localStorage.removeItem(REFRESH_TOKEN_KEY);
+}
+
+export function setRefreshToken(refreshToken) {
+    localStorage.setItem(REFRESH_TOKEN_KEY, refreshToken);
+}

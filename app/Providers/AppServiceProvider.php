4e75e5402b512583a33497990a5c1a7d2f69c8ed
Luca Casella <filoluca92@gmail.com>
2023-02-28 16:17:31 +0100

    bind

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register():void
     {
-        //
+
+        $this->app->bind(PassportClientCreatorContract::class, PassportClientCreator::class);
+
     }
 
     /**

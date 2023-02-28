a65b0f909081d0009e555b4e8c5686c7e8c013c2
Luca Casella <filoluca92@gmail.com>
2023-02-28 16:16:10 +0100

    implemented command qrmenu:create_client

---
+++ app/Console/Commands/Qrmenu_create_client.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+
+use App\Services\PassportClientCreator;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+
+class Qrmenu_create_client extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'qrmenu:create_client';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create client and store it in database';
+
+    protected $passportClientCreator;
+
+    public function __construct(PassportClientCreator $clientCreator)
+    {
+            parent::__construct();
+            $this->passportClientCreator = $clientCreator;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+      $this->passportClientCreator->create(env('REACT_APP_CLIENT_ID'), env('REACT_APP_CLIENT_SECRET'), env('REACT_APP_CLIENT_CALLBACK'), Str::random(8));
+
+      return Command::SUCCESS;
+
+    }
+
+}
---
+++ app/Services/PassportClientCreator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\Contracts\PassportClientCreatorContract;
+use Illuminate\Support\Facades\DB;
+use function PHPUnit\Framework\isNull;
+
+class PassportClientCreator implements PassportClientCreatorContract
+{
+
+    const TABLE_NAME = 'oauth_clients';
+
+    public function create(string $id, string $secret, string $callbackUrl, ?string $name = null): void
+    {
+        $client = DB::table(self::TABLE_NAME)
+            ->where(['id' => $id,
+                'secret' => $secret,
+                'redirect' => $callbackUrl,
+                'name' => $name])
+            ->first();
+
+        if ($client != null) {
+            return;
+        }
+        else {
+            DB::table(self::TABLE_NAME)
+                ->insert(['id' => $id,
+                    'secret' => $secret,
+                    'redirect' => $callbackUrl,
+                    'name' => $name,
+                    'personal_access_client' => 0,
+                    'password_client' => 0,
+                    'revoked' => 0,
+                    'created_at' => date('c'),
+                    'updated_at' => date('c')
+                ]);
+        }
+    }
+}

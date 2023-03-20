1ec702c6d2e7bd38241b5a927d253dbe543b4215
Antonio Bruno <abruno@artiso.com>
2023-03-20 11:06:35 +0100

    refactor set client from config command

--- app/Console/Commands/SetClientFromConfig.php
+++ app/Console/Commands/SetClientFromConfig.php
@@ -3,10 +3,14 @@
 namespace App\Console\Commands;
 
 use App\Services\Contracts\PassportClientCreatorContract;
-use Dotenv\Dotenv;
+use App\Traits\WorkWithEnvVars;
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Command\Command as CommandAlias;
 
 class SetClientFromConfig extends Command {
+
+    use WorkWithEnvVars;
+
     private PassportClientCreatorContract $clientCreator;
 
     public function __construct( PassportClientCreatorContract $clientCreator ) {
@@ -26,7 +30,7 @@ class SetClientFromConfig extends Command {
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Write an oauth client into the database, according to the .env configuration of the projects.';
 
     /**
      * Execute the console command.
@@ -37,57 +41,32 @@ class SetClientFromConfig extends Command {
 
         if ( file_exists( 'resources/app/.env' ) ) {
 
+            $this->loadPrivateAreaEnvVars();
+
             $this->info( 'File .env for React app exists' );
 
-            $dotenv = Dotenv::createImmutable( 'resources\app' );
-            $dotenv->load();
+            $privatearea_url = ($this->react_app_client_host == $this->laravel_app_host) ? $this->react_app_sub_folder : $this->react_app_client_host;
 
-            $react_app_sub_folder    = env( 'REACT_APP_SUBFOLDER_NAME' );
-            $react_app_client_id     = env( 'REACT_APP_CLIENT_ID' );
-            $react_app_client_secret = env( 'REACT_APP_CLIENT_SECRET' );
-            $react_app_client_host   = env( 'REACT_APP_HOST' );
-            $laravel_app_host        = env( 'REACT_APP_URL' );
+            $react_app_callback = $this->react_app_client_host;
 
-            $privatearea_url = ($react_app_client_host == $laravel_app_host) ? $react_app_sub_folder : $react_app_client_host;
+            if ( $this->react_app_sub_folder ) {
 
-            $react_app_callback = "{$react_app_client_host}";
-
-            if ( $react_app_sub_folder ) {
-
-                $react_app_callback .= "/{$react_app_sub_folder}";
+                $react_app_callback .= "/$this->react_app_sub_folder";
 
             }
 
-            $this->clientCreator->upsert( $react_app_client_id, $react_app_client_secret, $react_app_callback, 'REACT_APP' );
+            $this->clientCreator->upsert( $this->react_app_client_id, $this->react_app_client_secret, $react_app_callback, 'REACT_APP' );
 
-            $this->info( "Client {$react_app_client_id} created and/or updated" );
+            $this->info( "Client $this->react_app_client_id created and/or updated" );
 
-            $this->changeEnv( 'PRIVATEAREA_CLIENT_ID', $react_app_client_id );
-            $this->changeEnv( 'PRIVATEAREA_CLIENT_SECRET', $react_app_client_secret );
-            $this->changeEnv( 'PRIVATEAREA_CLIENT_CALLBACK', $react_app_callback );
-            $this->changeEnv( 'PRIVATEAREA_URL', $privatearea_url );
+            $this->changeLaravelEnv( 'PRIVATEAREA_CLIENT_ID', $this->react_app_client_id );
+            $this->changeLaravelEnv( 'PRIVATEAREA_CLIENT_SECRET', $this->react_app_client_secret );
+            $this->changeLaravelEnv( 'PRIVATEAREA_CLIENT_CALLBACK', $react_app_callback );
+            $this->changeLaravelEnv( 'PRIVATEAREA_URL', $privatearea_url );
 
             $this->info( "Server env vars updated!!" );
         }
 
-        return Command::SUCCESS;
-    }
-
-    public function changeEnv( $key, $value ) {
-        $path = base_path( '.env' );
-
-        if ( is_bool( env( $key ) ) ) {
-            $old = env( $key ) ? 'true' : 'false';
-        } elseif ( env( $key ) === null ) {
-            $old = 'null';
-        } else {
-            $old = env( $key );
-        }
-
-        if ( file_exists( $path ) ) {
-            file_put_contents( $path, str_replace(
-                "$key=" . $old, "$key=" . $value, file_get_contents( $path )
-            ) );
-        }
+        return CommandAlias::SUCCESS;
     }
 }

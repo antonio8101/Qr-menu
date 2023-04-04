b76e53fc1beb4615b24ba409636f9eb34ec643be
Manuel Raso <rasomanuel0@gmail.com>
2023-04-04 23:30:47 +0200

    add factory to seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Menu;
+use App\Models\Product;
+use App\Models\Section;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(1)->create();
+       // \App\Models\User::factory(1)->create()//;
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Menu::factory(1)->create();
+
+        Section::factory(1)->create();
+
+        Product::factory(1)->create();
     }
 }

d4774bdc481f62a8dd4916ee5d6747399f7b108b
Antonio <tessitoreantonio098@gmail.com>
2023-04-19 17:15:35 +0200

    new menu button with animation

--- resources/app/src/components/Sidebar.js
+++ resources/app/src/components/Sidebar.js
@@ -1,25 +1,34 @@
-import React, { useState } from "react";
-import { Icon } from "@iconify/react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import {Menu} from "./Menu";
 import {ButtonLogout} from "./ButtonLogout";
 
 export function Sidebar() {
     const [show, setShow] = useState(false);
-
+    const [click, setClick] = useState(false);
     return (
         <>
             <MenuButtonContainer>
-                <button onClick={() => setShow(!show)}>
-                    <Icon icon="mdi:menu" />
+                <button onClick={() => {
+                    setClick(false);
+                    setShow(!show);
+                    setClick(true);
+                }}>
+
+                    <span
+                        className={show && click ? "menu-button forward_rotation1" : !show && click ? "menu-button back_rotation1" : "menu-button"}></span>
+                    <span className={show ? "hide" : "menu-button center-bar"}></span>
+                    <span
+                        className={show && click ? "menu-button forward_rotation2" : !show && click ? "menu-button back_rotation2" : "menu-button"}></span>
                 </button>
             </MenuButtonContainer>
-            {show && (
-                <div className="navbar-show h-100 ">
-                    <Menu setShow={setShow} />
-                    <ButtonLogout />
-                </div>
-            )}
+            <div className={show ? "navbar show h-100 " : "navbar hide h-100 "}>
+                <Menu setShow={setShow}/>
+                <ButtonLogout/>
+
+
+            </div>
+
         </>
     );
 }
@@ -31,7 +40,6 @@ const MenuButtonContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-
     position: fixed;
     z-index: 99;
     top: 0;
@@ -46,7 +54,7 @@ const MenuButtonContainer = styled.div`
         width: 100%;
         height: 100%;
         * {
-            margin: 0;
+
             padding: 0;
         }
     }

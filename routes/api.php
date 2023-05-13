2a2d645e2877a8cc82dd66d5fe7370fa04ae1235
Luca <filoluca92@gmail.com>
2023-05-13 17:07:12 +0200

    setted new Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\MenuAPI\MenuController;
+use App\Http\Controllers\MenuAPI\ProductController;
+use App\Http\Controllers\MenuAPI\SectionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +20,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('menu/',[MenuController::class,'index']);
-Route::post('menu/create',[MenuController::class,'create']);
 
+Route::get('menu/',[MenuController::class,'index']);
+
+Route::middleware('auth:api')->post('menu/create',[MenuController::class,'create']);
+
+Route::middleware('auth:api')->post('menu/update', [MenuController::class, 'update']);
+
+Route::middleware('auth:api')->post('menu/delete', [MenuController::class, 'destroy']);
+
+Route::middleware('auth:api')->post('section/create', [SectionController::class, 'create']);
+
+Route::middleware('auth:api')->post('section/update', [SectionController::class, 'update']);
+
+Route::middleware('auth:api')->post('section/delete', [SectionController::class, 'destroy']);
+
+Route::middleware('auth:api')->post('product/create', [ProductController::class, 'create']);
+
+Route::middleware('auth:api')->post('product/update', [ProductController::class, 'update']);
+
+Route::middleware('auth:api')->post('product/delete', [ProductController::class, 'destroy']);

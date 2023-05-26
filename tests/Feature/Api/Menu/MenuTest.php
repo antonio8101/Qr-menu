5c4f818e76dca3138b35fedb82da868916037ec4
Antonio Bruno <abruno@artiso.com>
2023-05-26 19:03:55 +0200

    we don't want to be bother by created_at assignment

--- tests/Feature/Api/Menu/MenuTest.php
+++ tests/Feature/Api/Menu/MenuTest.php
@@ -44,4 +44,37 @@ class MenuTest extends TestCase {
         $this->assertTrue(is_array($getResult), "Next query should return an array");
         $this->assertTrue(in_array($nameOfTheMenuToCreate, array_map(fn ($x) => $x->$menuNameProp, $getResult)), "The result should contain our name key");
     }
+
+    /**
+     * @throws Throwable
+     */
+    public function test_menu_get(): void {
+
+        $createRoute = '/api/menu/create';
+        $getRoute = '/api/menu/';
+        $nameOfTheMenuToCreate = 'test';
+        $menuNameProp = Menu::NAME;
+
+        Passport::actingAs(
+            User::factory()->create(),
+        // define scope
+        );
+
+        $createResponse = $this
+            ->post($createRoute, [
+                'name_menu' => $nameOfTheMenuToCreate
+            ]);
+
+        $createResponse->assertStatus(204);
+        $createResponse->assertNoContent();
+
+        $getResponse = $this->get($getRoute);
+
+        $getResponse->assertStatus(200);
+        $getResult = json_decode($getResponse->getContent());
+
+        $this->assertTrue(is_array($getResult), "Next query should return an array");
+        $this->assertTrue(in_array($nameOfTheMenuToCreate, array_map(fn ($x) => $x->$menuNameProp, $getResult)), "The result should contain our name key");
+    }
+
 }

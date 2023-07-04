7cb1386973931abb018783a53674ae82d5c473d4
Luca <filoluca92@gmail.com>
2023-07-04 22:23:23 +0200

    Updated api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\MenuAPI\MenuController;
 use App\Http\Controllers\MenuAPI\ProductController;
 use App\Http\Controllers\MenuAPI\SectionController;
+use App\Models\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,24 +22,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->get('menu/',[MenuController::class,'index']);
+Route::middleware('auth:api')->get('menu/', [MenuController::class,'index']);
 
-Route::middleware('auth:api')->post('menu/create',[MenuController::class,'create']);
+Route::middleware('auth:api')->get('menu/{menu_id}', [MenuController::class,'show']);
 
-Route::middleware('auth:api')->post('menu/update', [MenuController::class, 'update']);
+Route::middleware('auth:api')->post('menu/create', [MenuController::class,'create']);
 
-Route::middleware('auth:api')->post('menu/delete', [MenuController::class, 'destroy']);
+Route::middleware('auth:api')->put('menu/update/{menu_id}', [MenuController::class, 'update']);
 
-Route::middleware('auth:api')->post('section/create', [SectionController::class, 'create']);
+Route::middleware('auth:api')->delete('menu/delete/{menu_id}', [MenuController::class, 'destroy']);
 
-Route::middleware('auth:api')->post('section/update', [SectionController::class, 'update']);
+Route::middleware('auth:api')->get('menu/{menu_id}/sections', [SectionController::class, 'index']);
 
-Route::middleware('auth:api')->post('section/delete', [SectionController::class, 'destroy']);
+Route::middleware('auth:api')->get('menu/{menu_id}/section/{section_id}', [SectionController::class, 'show']);
 
-Route::middleware('auth:api')->post('product/create', [ProductController::class, 'create']);
+Route::middleware('auth:api')->post('menu/{menu_id}/section/create', [SectionController::class, 'create']);
 
-Route::middleware('auth:api')->post('product/update', [ProductController::class, 'update']);
+Route::middleware('auth:api')->put('menu/{menu_id}/section/update/{section_id}', [SectionController::class, 'update']);
 
-Route::middleware('auth:api')->post('product/delete', [ProductController::class, 'destroy']);
+Route::middleware('auth:api')->delete('menu/{menu_id}/section/delete/{section_id}', [SectionController::class, 'destroy']);
 
-Route::middleware('auth:api')->get('menu/{id}',[MenuController::class,'show']);
+Route::middleware('auth:api')->get('menu/{menu_id}/section/{section_id}/products', [ProductController::class, 'index']);
+
+Route::middleware('auth:api')->get('menu/{menu_id}/section/{section_id}/product/{product_id}', [ProductController::class, 'show']);
+
+Route::middleware('auth:api')->post('menu/{menu_id}/section/{section_id}/product/create', [ProductController::class, 'create']);
+
+Route::middleware('auth:api')->put('menu/{menu_id}/section/{section_id}/product/update/{product_id}', [ProductController::class, 'update']);
+
+Route::middleware('auth:api')->delete('menu/{menu_id}/section/{section_id}/product/delete/{product_id}', [ProductController::class, 'destroy']);
